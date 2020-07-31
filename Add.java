0153926a894517010b7188737b367a9ca9d453db
Your Name <meghadixit@qainfotech.com>
2020-07-31 17:00:41 +0530

    Add Program

---
+++ Add.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Add
+{
+public static void main(String args[])
+{
+Scanner ob = new Scanner(System.in);
+int n1=ob.nextInt();
+int n2=ob.nextInt();
+System.out.println(n1+n2);
+}
+}
